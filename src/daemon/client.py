d4d281403b3ea0da86c828b535e08753a49e3e49
ma-anwar <ma.rizvi.anwar@gmail.com>
2022-11-21 21:17:09 -0500

    Handle registration flow succesfully

--- src/daemon/client.py
+++ src/daemon/client.py
@@ -1,5 +1,6 @@
 """This module represents a client and defines client message handlers"""
 import logging
+from selectors import SelectorKey
 
 import constants
 from message import Message
@@ -8,16 +9,44 @@ from message import Message
 class Client:
     """Class responsible for handling messages related to client connections"""
 
-    def __init__(self):
-        self.logger = logging.getLogger("client")
-        self.is_registered = False
-        self.nick = b""
-        self.real_name = b""
-        self.user_name = b""
+    def __init__(self, address: str, key: SelectorKey):
+        self._logger = logging.getLogger("client")
+        self._is_registered = False
+        self._nick = ""
+        self._real_name = ""
+        self._username = ""
+        self._key = key
+        self.address = address
+
+    @property
+    def is_registered(self):
+        """Get value of _is_registered"""
+        return self._is_registered
+
+    @is_registered.setter
+    def is_registered(self, is_registered):
+        """Set value of is_registered, call success handler if is_registered"""
+        self._is_registered = is_registered
+        if is_registered:
+            self._send_registration_success()
+
+    def _send_registration_success(self):
+        self._key.data.out_buffer += (
+            b"001 %s: Hi, welcome to our PyIrcd\r\n" % self._nick.encode()
+        )
+        self._key.data.out_buffer += (
+            b"002 %s: Your host is PyIrcd, running version 1\r\n" % self._nick.encode()
+        )
+        self._key.data.out_buffer += (
+            b"003 %s: This server is being created\r\n" % self._nick.encode()
+        )
+        self._key.data.out_buffer += (
+            b"004 %s: This server is being created\r\n" % self._nick.encode()
+        )
 
     def handle_message(self, message: Message):
         """Handle message by invoking registration flow"""
-        self.logger.debug(message)
+        self._logger.debug(message)
         if not self.is_registered:
             self.handle_registration_flow(message)
 
@@ -26,15 +55,15 @@ class Client:
         # WIP registration flow
         # Need to handle error cases
         if message.command == constants.NICK_COMMAND and message.parameters[0]:
-            self.nick = message.parameters[0]
+            self._nick = message.parameters[0]
 
         if message.command == constants.USER_COMMAND:
             if len(message.parameters) < 4:
-                self.logger.info("ERROR")
+                self._logger.info("ERROR")
 
-            self.user_name = message.parameters[0]
-            self.real_name = message.parameters[3]
+            self._username = message.parameters[0]
+            self._real_name = message.parameters[3]
 
         # user_name implies real_name is present
-        if self.user_name and self.nick:
+        if self._username and self._nick:
             self.is_registered = True
