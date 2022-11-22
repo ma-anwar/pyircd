b73b6438f265a3f5a26536a3b50570ca86409cf2
serhatgktp <serhatgktp@hotmail.com>
2022-11-22 02:34:44 -0500

    Parser cleanup, added constant for max line size

--- src/daemon/constants.py
+++ src/daemon/constants.py
@@ -7,6 +7,7 @@ EMPTY_STRING = b""
 # Most IRC servers limit messages to 512 bytes in length
 # https://modern.ircdocs.horse/#message-format
 RECEIVE_LENGTH = 1024
+MAX_LINE_LENGTH = 512
 
 # +1 for \n since find gives us index of \r
 # +1 bc end index is not inclusive
--- src/daemon/parser.py
+++ src/daemon/parser.py
@@ -26,25 +26,44 @@ class Parser:
         self.__dispatch(parsed_message)
 
     def dispatch(self, message: Message):
-        """Call handler on message, used by Server"""
+        # Currently, the delimiter check is being done inside server.py
+        # If the delimiter is not found, the message never reaches the parser
         self.__handle_message(message)
 
-    # Jank temporary parse function, not to spec, just for PoC
+    # Work-in-progress, INCOMPLETE
     def __parse_message(self, message: Message) -> Message:
         """Parse message according to IRC spec"""
+        # Decode message
+        message_str = message.message.decode("utf-8")
+
         # Remove EOL delimiter
         delimiter_length = len(constants.IRC_TERMINATION_DELIMITER)
-        stripped_message = message.message[:-delimiter_length]
+        stripped_message = message_str[:-delimiter_length]
 
+        # Check whether the message length exceeds the max length
+        if len(stripped_message) > constants.MAX_LINE_LENGTH - 2:
+            ret_msg = stripped_message[: constants.MAX_LINE_LENGTH - 2] + "\r\n"
+        else:
+            ret_msg = stripped_message + "\r\n"
+
+        # Check for prefix?
+        # A prefix could be used to invoke a command instead of sending a message
+
+        # Check command
         split_message = stripped_message.split()
 
+        # Check params
         command = split_message.pop(0)
         parameters = split_message
 
+        # Encode message
+        ret_msg = ret_msg.encode("utf-8")
+
+        # Return parsed message
         parsed_message = Message(
             message.client_address,
             "HANDLE",
-            message.message,
+            ret_msg,
             message.key,
             command,
             parameters,
