3fcb66b494499a9019ac34261886a0a2e86acf5e
serhatgktp <serhatgktp@hotmail.com>
2022-11-24 16:25:49 -0500

    Fixed run() signature, added .toml file for irctest

--- src/test/pyircd_controller.py
+++ src/test/pyircd_controller.py
@@ -12,6 +12,8 @@ class PyIrcdController(BaseServerController):
     supported_sasl_mechanisms = set(
         os.environ.get("IRCTEST_SERVER_SASL_MECHS", "").split()
     )
+    port = 6667
+    hostname = "localhost"
 
     def kill_proc(self) -> None:
         """Kills the process"""
@@ -27,9 +29,24 @@ class PyIrcdController(BaseServerController):
             )
         return (hostname, int(port))
 
-    def run(self, host="127.0.0.1", port="6667") -> None:
+    def run(
+        self,
+        host="127.0.0.1",
+        port="6667",
+        password=None,
+        valid_metadata_keys=None,
+        invalid_metadata_keys=None,
+        ssl=None,
+        run_services=None,
+        faketime=None,
+    ) -> None:
         """Run the pyircd server"""
-        self.proc = subprocess.run("poetry", f"run python src/daemon/daemon.py --host {host} --port {port}")
+        run_daemon_command = (
+            "poetry run python "
+            + "/home/efkan/Documents/D58/final-project/pyircd/src/daemon/daemon.py "
+            + f"--host {host} --port {port}"
+        )
+        self.proc = subprocess.Popen(run_daemon_command.split())
 
 
 def get_irctest_controller_class() -> Type[PyIrcdController]:
