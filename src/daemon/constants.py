d26de9169d6b1963f486c50d9949570dd32c60a4
Anwar <ma.rizvi.anwar@gmail.com>
2022-11-24 20:15:43 -0500

    Implement Basic Registration Flow (#6)
    * Fix private function naming convention to use _
    
    * Fix message type
    
    * Dispatch messages for every IRC message in in_buffer
    
    * Handle registration flow succesfully
    
    * Clean up sending client messages, add logging config
    
    * Update README
    
    * Update poetry dependencies
    
    * Update lock file after rebase
    
    * Rename EventBus to MessageBus

--- src/daemon/constants.py
+++ src/daemon/constants.py
@@ -1,17 +1,39 @@
 """This module holds constants used throughout the daemon"""
+from enum import Enum, unique
 
-NICK_COMMAND = b"NICK"
-USER_COMMAND = b"USER"
+
+# Use str as value in enums: https://docs.python.org/3/library/enum.html#notes
+@unique
+class IRC_COMMANDS(str, Enum):
+    NICK = "NICK"
+    USER = "USER"
+    PASS = "PASS"
+
+
+# https://modern.ircdocs.horse/#numerics
+@unique
+class IRC_REPLIES(str, Enum):
+    WELCOME = "001"
+    YOURHOST = "002"
+    CREATED = "003"
+    MYINFO = "004"
+
+
+# https://modern.ircdocs.horse/#message-format
 IRC_TERMINATION_DELIMITER = b"\r\n"
+
 EMPTY_STRING = b""
+
 # Most IRC servers limit messages to 512 bytes in length
 # https://modern.ircdocs.horse/#message-format
 RECEIVE_LENGTH = 1024
 
 # +1 for \n since find gives us index of \r
 # +1 bc end index is not inclusive
-DELIMITER_END_INDEX = 2
+DELIMITER_END_OFFSET = 2
 
+# -1 is returned by find when not found
 NOT_FOUND = -1
+
 LOCAL_HOST = "127.0.0.1"
 DEFAULT_PORT = 6667
