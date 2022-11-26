1fc3b743410733cd73dd5c31dd67307259ee6ea4
Efkan S. Goktepe <76130958+serhatgktp@users.noreply.github.com>
2022-11-26 01:43:40 -0500

    Dev (#10)
    * Parser (#7)
    
    Implemented parser and added alphabetical commands constant
    
    * Remove magic numbers, fix message return value (#9)
    
    Co-authored-by: Anwar <ma.rizvi.anwar@gmail.com>

--- src/daemon/constants.py
+++ src/daemon/constants.py
@@ -27,6 +27,7 @@ EMPTY_STRING = b""
 # Most IRC servers limit messages to 512 bytes in length
 # https://modern.ircdocs.horse/#message-format
 RECEIVE_LENGTH = 1024
+MAX_LINE_LENGTH = 512
 
 # +1 for \n since find gives us index of \r
 # +1 bc end index is not inclusive
@@ -37,3 +38,48 @@ NOT_FOUND = -1
 
 LOCAL_HOST = "127.0.0.1"
 DEFAULT_PORT = 6667
+
+ACCEPTED_ACTIONS = ["PARSE"]
+
+# Based on https://modern.ircdocs.horse/#client-messages
+VALID_ALPHA_COMMANDS = [
+    "CAP",
+    "AUTHENTICATE",
+    "PASS",
+    "NICK",
+    "USER",
+    "PING",
+    "PONG",
+    "OPER",
+    "QUIT",
+    "ERROR",
+    "JOIN",
+    "PART",
+    "TOPIC",
+    "NAMES",
+    "LIST",
+    "INVITE",
+    "KICK",
+    "MOTD",
+    "VERSION",
+    "ADMIN",
+    "CONNECT",
+    "TIME",
+    "STATS",
+    "HELP",
+    "INFO",
+    "MODE",
+    "PRIVMSG",
+    "NOTICE",
+    "WHO",
+    "WHOIS",
+    "WHOWAS",
+    "KILL",
+    "REHASH",
+    "RESTART",
+    "SQUIT",
+    "AWAY",
+    "LINKS",
+    "USERHOST",
+    "WALLOPS",
+]
--- src/daemon/parser.py
+++ src/daemon/parser.py
@@ -15,34 +15,76 @@ class Parser:
 
     def __init__(self, dispatch: callable):
         """Save the dispatch function to send messages to event_bus"""
-        self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger("Parser")
         self._dispatch = dispatch
 
     def _handle_message(self, message: Message):
         """Handle Message and dispatch it to EventBus"""
         self.logger.debug(message)
+        if message.action not in constants.ACCEPTED_ACTIONS:
+            return
         parsed_message = self._parse_message(message)
         self.logger.debug(parsed_message)
-        self._dispatch(parsed_message)
+        if parsed_message is not None:
+            self._dispatch(parsed_message)
 
     def dispatch(self, message: Message):
-        """Call handler on message, used by Server"""
+        """Receive incoming message at parser"""
+        # Currently, the delimiter check is being done inside server.py
+        # If the delimiter is not found, the message never reaches the parser
         self._handle_message(message)
 
-    # Jank temporary parse function, not to spec, just for PoC
+    def _parse_parameters(self, parameters: list):
+        """Return a list of parsed parameters.
+        If there are any bad parameters, return None"""
+        valid_parameters = []
+        join_params = False
+        for i in range(len(parameters)):
+            if parameters[i][0] == ":":
+                join_params = True
+                parameters[i] = parameters[i][1:]
+                parameters[i] = " ".join(parameters[i:])
+            for forbidden_sequence in ["\0", "\r", "\n", "::"]:
+                if forbidden_sequence in parameters[i]:
+                    return None
+            valid_parameters.append(parameters[i])
+            if join_params:
+                break
+        return valid_parameters
+
     def _parse_message(self, message: Message) -> Message:
-        """Parse message according to IRC spec"""
-        decoded_message = message.message.decode("utf-8")
+        """Parse message according to IRC spec
+        If parse is successful, forward message to EventBus
+        Otherwise, drop message"""
+
+        # Decode message
+        message_str = message.message.decode("utf-8")
 
         # Remove EOL delimiter
         delimiter_length = len(constants.IRC_TERMINATION_DELIMITER)
-        stripped_message = decoded_message[:-delimiter_length]
+        stripped_message = message_str[:-delimiter_length]
 
+        # Accept messages that exceed MAX_LINE_LENGTH, but slice them
+        # https://modern.ircdocs.horse/#compatibility-with-incorrect-software
+        if len(stripped_message) > constants.MAX_LINE_LENGTH - delimiter_length:
+            stripped_message = stripped_message[
+                : constants.MAX_LINE_LENGTH - delimiter_length
+            ]
+
+        # Split message by space
         split_message = stripped_message.split()
 
-        command = split_message.pop(0)
-        parameters = split_message
+        # Inspect contents (assume no tags and source for now)
+        if len(split_message) < 1:
+            return
 
+        command = split_message[0].upper()
+        parameters = self._parse_parameters(split_message[1:])
+
+        if (command not in constants.VALID_ALPHA_COMMANDS) or parameters is None:
+            return
+
+        # Return parsed message
         parsed_message = Message(
             message.client_address,
             "HANDLE",
